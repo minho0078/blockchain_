8c2c42508e15dcd59cf55e81ec6fd94672ce0fbb
minho0078 <minho0078@gmail.com>
2018-06-18 13:56:22 +0900

    package.json 과 tsconfig.json ADD!!

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
-};
+class Human{
+    public name: string;
+    public age: number;
+    public gender: string;
 
-const person = {
-    name : "minho",
-    age : 36,
-    gender : "male"
-};
+    constructor(name: string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const minho = new Human("minho",36,"male");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(minho));
 export{};
